36d082abb07c62e42eae5d061a88177bef07ebc9
IvanShimanovich <shmel38@gmail.com>
2015-09-26 19:42:22 +0300

    Changed form from default Form1 to Main form. Added buttons and picture box. Loading of images.

---
+++ FaceDetector/MainForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace FaceDetector
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainControlPanel = new System.Windows.Forms.Panel();
+            this.ProcessButton = new System.Windows.Forms.Button();
+            this.LoadImageButton = new System.Windows.Forms.Button();
+            this.OpenImageFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.MainPictureBox = new System.Windows.Forms.PictureBox();
+            this.mainControlPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainControlPanel
+            // 
+            this.mainControlPanel.Controls.Add(this.ProcessButton);
+            this.mainControlPanel.Controls.Add(this.LoadImageButton);
+            this.mainControlPanel.Location = new System.Drawing.Point(545, 12);
+            this.mainControlPanel.Name = "mainControlPanel";
+            this.mainControlPanel.Size = new System.Drawing.Size(200, 337);
+            this.mainControlPanel.TabIndex = 0;
+            // 
+            // ProcessButton
+            // 
+            this.ProcessButton.Enabled = false;
+            this.ProcessButton.Location = new System.Drawing.Point(3, 51);
+            this.ProcessButton.Name = "ProcessButton";
+            this.ProcessButton.Size = new System.Drawing.Size(194, 32);
+            this.ProcessButton.TabIndex = 1;
+            this.ProcessButton.Text = "Process";
+            this.ProcessButton.UseVisualStyleBackColor = true;
+            this.ProcessButton.Click += new System.EventHandler(this.ProcessButton_Click);
+            // 
+            // LoadImageButton
+            // 
+            this.LoadImageButton.Location = new System.Drawing.Point(3, 3);
+            this.LoadImageButton.Name = "LoadImageButton";
+            this.LoadImageButton.Size = new System.Drawing.Size(194, 33);
+            this.LoadImageButton.TabIndex = 0;
+            this.LoadImageButton.Text = "Load Image";
+            this.LoadImageButton.UseVisualStyleBackColor = true;
+            this.LoadImageButton.Click += new System.EventHandler(this.LoadImageButton_Click);
+            // 
+            // OpenImageFileDialog
+            // 
+            this.OpenImageFileDialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF";
+            // 
+            // MainPictureBox
+            // 
+            this.MainPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.MainPictureBox.Name = "MainPictureBox";
+            this.MainPictureBox.Size = new System.Drawing.Size(516, 337);
+            this.MainPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.MainPictureBox.TabIndex = 1;
+            this.MainPictureBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 361);
+            this.Controls.Add(this.MainPictureBox);
+            this.Controls.Add(this.mainControlPanel);
+            this.Name = "MainForm";
+            this.Text = "Face Detector";
+            this.mainControlPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainControlPanel;
+        private System.Windows.Forms.Button LoadImageButton;
+        private System.Windows.Forms.OpenFileDialog OpenImageFileDialog;
+        private System.Windows.Forms.PictureBox MainPictureBox;
+        private System.Windows.Forms.Button ProcessButton;
+    }
+}
+
---
+++ FaceDetector/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaceDetector
+{
+    /// <summary>
+    /// Main Form of application. 
+    /// Contains controls for loading of images and running face detection.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+
+        /// <summary>
+        /// Constructor w/o parameters.
+        /// </summary>
+        /// <see cref="MainForm"/>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Event handler on Load Image button click. Shows dialog for selection file to detect.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LoadImageButton_Click(object sender, EventArgs e)
+        {
+            if (OpenImageFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Stream fileStream = OpenImageFileDialog.OpenFile();
+                if (fileStream != null)
+                {
+                    Bitmap image = new Bitmap(fileStream);
+                    MainPictureBox.Image = image;
+                    ProcessButton.Enabled = true;
+                }
+            }
+        }
+
+        private void ProcessButton_Click(object sender, EventArgs e)
+        {
+            if (MainPictureBox.Image != null) 
+            {
+                
+            }
+            else
+            {
+                MessageBox.Show("There is no image to process. Load it and try again.");
+            }
+        }
+    }
+}
