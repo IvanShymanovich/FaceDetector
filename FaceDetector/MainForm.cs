be3750196c30b1f18afda30e2a2b14c56a14058f
IvanShymanovich <ivan.shymanovich@gmail.com>
2015-10-25 15:53:35 +0300

    moved accord implementation as is. changes namespaces.

--- FaceDetector/MainForm.cs
+++ FaceDetector/MainForm.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using FaceDetector.Filter;
+using FaceDetector.Detection;
+using FaceDetector.Cascades;
 
 namespace FaceDetector
 {
@@ -19,6 +21,7 @@ namespace FaceDetector
     public partial class MainForm : Form
     {
         private Bitmap image;
+        private HaarObjectDetector detector;
         /// <summary>
         /// Constructor w/o parameters.
         /// </summary>
@@ -26,6 +29,8 @@ namespace FaceDetector
         public MainForm()
         {
             InitializeComponent();
+            HaarCascade cascade = new FaceHaarCascade();
+            detector = new HaarObjectDetector(cascade, 30);
         }
 
         /// <summary>
@@ -51,14 +56,14 @@ namespace FaceDetector
         {
             if (image != null)
             {
-
+                
                 if (MedianFilterCB.Checked) 
                     image = new MedianFilter().ApplyMedianFilter(image, (int)medianUpDown.Value);
                 if (GaussianBlurFilterCB.Checked)
                     image = new GaussianBlurFilter().ApplyGaussianBlur(image,
                         new Rectangle(0, 0, image.Width, image.Height), (int)gaussianUpDown.Value);
                 
-                if (ContrastCB.Checked && contrast_textBox.Text != "100")
+                /*if (ContrastCB.Checked && contrast_textBox.Text != "100")
                 {
                     int percent = Int32.Parse(contrast_textBox.Text);
                     image = new ContrastFilter().apply2(image, new Rectangle(0, 0, image.Width, image.Height), percent);
@@ -70,14 +75,37 @@ namespace FaceDetector
                     image = new ColorMatchingFilter().apply(image, (int)red_numeric.Value, (int)green_numeric.Value, 
                         (int)blueNumeric.Value);
                 }
-                
+                */
 
-                MainPictureBox.Image = image;
+                detector.SearchMode = ObjectDetectorSearchMode.NoOverlap;
+                detector.ScalingMode = ObjectDetectorScalingMode.SmallerToGreater;
+                detector.ScalingFactor = 1.5f;
+                detector.UseParallelProcessing = true;
+
+                Rectangle[] objects = detector.ProcessFrame(image);
+
+                if (objects.Length > 0)
+                {
+                    RectanglesMarker marker = new RectanglesMarker(objects, Color.Fuchsia);
+                    image = ConvertToFormat(image, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                    MainPictureBox.Image = marker.Apply(image);
+                }
+
+                //MainPictureBox.Image = image;
             }
             else
             {
                 MessageBox.Show("There is no image to process. Load it and try again.");
             }
+        }
+        private static Bitmap ConvertToFormat(Bitmap source, System.Drawing.Imaging.PixelFormat format)
+        {
+            Bitmap copy = new Bitmap(source.Width, source.Height, format);
+            using (Graphics gr = Graphics.FromImage(copy))
+            {
+                gr.DrawImage(source, new Rectangle(0, 0, copy.Width, copy.Height));
+            }
+            return copy;
         }
 
     }
